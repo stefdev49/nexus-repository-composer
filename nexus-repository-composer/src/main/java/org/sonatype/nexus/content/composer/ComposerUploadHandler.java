6222b42a7961d3a438e78156b547e2dd69105c76
Stef <stef.dev.49@gmail.com>
2023-08-05 08:02:44 +0200

    renamed raw to composer

---
+++ nexus-repository-composer/src/main/java/org/sonatype/nexus/content/composer/ComposerUploadHandler.java
@@ -0,0 +1,94 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.content.composer;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.sonatype.nexus.repository.Repository;
+import org.sonatype.nexus.repository.importtask.ImportFileConfiguration;
+import org.sonatype.nexus.repository.composer.ComposerUploadHandlerSupport;
+import org.sonatype.nexus.repository.composer.internal.ComposerFormat;
+import org.sonatype.nexus.repository.rest.UploadDefinitionExtension;
+import org.sonatype.nexus.repository.security.ContentPermissionChecker;
+import org.sonatype.nexus.repository.security.VariableResolverAdapter;
+import org.sonatype.nexus.repository.view.Content;
+import org.sonatype.nexus.repository.view.PartPayload;
+import org.sonatype.nexus.repository.view.payloads.TempBlob;
+import org.sonatype.nexus.repository.view.payloads.TempBlobPayload;
+
+import com.google.common.collect.Lists;
+
+/**
+ * Support for uploading raw components via UI & API
+ *
+ * @since 3.24
+ */
+@Named(ComposerFormat.NAME)
+@Singleton
+public class ComposerUploadHandler
+    extends ComposerUploadHandlerSupport
+{
+  @Inject
+  public ComposerUploadHandler(final ContentPermissionChecker contentPermissionChecker,
+                          @Named("simple") final VariableResolverAdapter variableResolverAdapter,
+                          final Set<UploadDefinitionExtension> uploadDefinitionExtensions)
+  {
+    super(contentPermissionChecker, variableResolverAdapter, uploadDefinitionExtensions, true);
+  }
+
+  @Override
+  protected List<Content> getResponseContents(final Repository repository, final Map<String, PartPayload> pathToPayload)
+      throws IOException
+  {
+    ComposerContentFacet facet = repository.facet(ComposerContentFacet.class);
+
+    List<Content> responseContents = Lists.newArrayList();
+    for (Entry<String,PartPayload> entry : pathToPayload.entrySet()) {
+      String path = entry.getKey();
+
+      Content content = facet.put(path, entry.getValue());
+
+      responseContents.add(content);
+    }
+    return responseContents;
+  }
+
+  @Override
+  protected Content doPut(final ImportFileConfiguration configuration) throws IOException {
+    Repository repository = configuration.getRepository();
+    String path = configuration.getAssetName();
+    Path contentPath = configuration.getFile().toPath();
+
+    ComposerContentFacet contentFacet = repository.facet(ComposerContentFacet.class);
+    String contentType = Files.probeContentType(contentPath);
+    try (TempBlob blob = contentFacet.blobs().ingest(contentPath, contentType, ComposerContentFacet.HASHING,
+        configuration.isHardLinkingEnabled())) {
+      return contentFacet.put(path, new TempBlobPayload(blob, contentType));
+    }
+  }
+
+  @Override
+  protected String normalizePath(final String path) {
+    return "/" + super.normalizePath(path);
+  }
+}
---
+++ nexus-repository-composer/src/main/java/org/sonatype/nexus/orient/composer/ComposerUploadHandler.java
@@ -0,0 +1,109 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.orient.composer;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.sonatype.nexus.mime.MimeSupport;
+import org.sonatype.nexus.orient.composer.internal.ComposerContentFacetImpl;
+import org.sonatype.nexus.repository.Repository;
+import org.sonatype.nexus.repository.importtask.ImportFileConfiguration;
+import org.sonatype.nexus.repository.composer.ComposerUploadHandlerSupport;
+import org.sonatype.nexus.repository.composer.internal.ComposerFormat;
+import org.sonatype.nexus.repository.rest.UploadDefinitionExtension;
+import org.sonatype.nexus.repository.security.ContentPermissionChecker;
+import org.sonatype.nexus.repository.security.VariableResolverAdapter;
+import org.sonatype.nexus.repository.storage.StorageFacet;
+import org.sonatype.nexus.repository.view.Content;
+import org.sonatype.nexus.repository.view.PartPayload;
+import org.sonatype.nexus.repository.view.payloads.TempBlob;
+import org.sonatype.nexus.repository.view.payloads.TempBlobPayload;
+import org.sonatype.nexus.transaction.UnitOfWork;
+
+import com.google.common.collect.Lists;
+
+/**
+ * Support for uploading components via UI & API
+ *
+ * @since 3.7
+ */
+@Named(ComposerFormat.NAME)
+@Singleton
+public class ComposerUploadHandler
+    extends ComposerUploadHandlerSupport
+{
+  private final MimeSupport mimeSupport;
+
+  @Inject
+  public ComposerUploadHandler(
+      final ContentPermissionChecker contentPermissionChecker,
+      @Named("simple") final VariableResolverAdapter variableResolverAdapter,
+      final Set<UploadDefinitionExtension> uploadDefinitionExtensions,
+      final MimeSupport mimeSupport)
+  {
+    super(contentPermissionChecker, variableResolverAdapter, uploadDefinitionExtensions, false);
+    this.mimeSupport = mimeSupport;
+  }
+
+  @Override
+  protected List<Content> getResponseContents(final Repository repository, final Map<String, PartPayload> pathToPayload)
+      throws IOException
+  {
+    ComposerContentFacet facet = repository.facet(ComposerContentFacet.class);
+
+    List<Content> responseContents = Lists.newArrayList();
+    UnitOfWork.begin(repository.facet(StorageFacet.class).txSupplier());
+    try {
+      for (Entry<String, PartPayload> entry : pathToPayload.entrySet()) {
+        String path = entry.getKey();
+
+        Content content = facet.put(path, entry.getValue());
+
+        responseContents.add(content);
+      }
+    }
+    finally {
+      UnitOfWork.end();
+    }
+    return responseContents;
+  }
+
+  @Override
+  protected Content doPut(final ImportFileConfiguration configuration) throws IOException {
+    Repository repository = configuration.getRepository();
+    String path = configuration.getAssetName();
+    Path contentPath = configuration.getFile().toPath();
+
+    try (TempBlob tempBlob = repository.facet(StorageFacet.class).createTempBlob(contentPath,
+        ComposerContentFacetImpl.HASH_ALGORITHMS, configuration.isHardLinkingEnabled());
+        InputStream in = new BufferedInputStream(Files.newInputStream(contentPath, StandardOpenOption.READ))) {
+      ComposerContentFacet facet = repository.facet(ComposerContentFacet.class);
+      facet.put(path,
+          new TempBlobPayload(tempBlob, mimeSupport.detectMimeType(in, contentPath.getFileName().toString())));
+      return facet.get(path);
+    }
+  }
+}
