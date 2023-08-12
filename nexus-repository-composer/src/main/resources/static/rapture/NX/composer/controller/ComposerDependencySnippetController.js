ee10ec81432aafee152b8d595a1d44afa29194c3
Stef <stef.dev.49@gmail.com>
2023-08-12 21:25:37 +0200

    try to add controller

--- nexus-repository-composer/src/main/resources/static/rapture/NX/composer/controller/ComposerDependencySnippetController.js
+++ nexus-repository-composer/src/main/resources/static/rapture/NX/composer/controller/ComposerDependencySnippetController.js
@@ -33,94 +33,18 @@ Ext.define('NX.composer.controller.ComposerDependencySnippetController', {
   snippetGenerator: function(componentModel, assetModel) {
     var group = componentModel.get('group'),
         name = componentModel.get('name'),
-        version = componentModel.get('version'),
-        attributes = assetModel && assetModel.get('attributes'),
-        maven2 = attributes && attributes.maven2,
-        classifier = maven2 && maven2.classifier,
-        extension = maven2 && maven2.extension,
-        gradleCoordinates;
-
-    gradleCoordinates = group + ':' + name + ':' + version +
-        (classifier ? ':' + classifier : '') +
-        (extension ? '@' + extension : '');
+        version = componentModel.get('version');
 
     return [
       {
-        displayName: 'Apache Maven',
-        description: 'Insert this snippet into your pom.xml',
+        displayName: 'Composer',
+        description: 'Insert this snippet into your composer.json',
         snippetText:
             '<dependency>\n' +
               '  <groupId>' + group + '</groupId>\n' +
               '  <artifactId>' + name + '</artifactId>\n' +
               '  <version>' + version + '</version>\n' +
-              (classifier ? '  <classifier>' + classifier + '</classifier>\n' : '') +
-              ((extension && 'jar' !== extension) ? '  <type>' + extension + '</type>\n' : '') +
             '</dependency>'
-      }, {
-        displayName: 'Gradle Groovy DSL',
-        snippetText:
-            'implementation \'' + gradleCoordinates + '\''
-      }, {
-        displayName: 'Gradle Kotlin DSL',
-        snippetText:
-            'implementation("' + gradleCoordinates + '")'
-      }, {
-        displayName: 'Scala SBT',
-        snippetText:
-            'libraryDependencies += "' + group + '" % "' + name + '" % "' + version + '"' +
-            (classifier ? ' classifier "' + classifier + '"' : '')
-      }, {
-        displayName: 'Apache Ivy',
-        snippetText:
-            '<dependency org="' + group + '" name="' + name + '" rev="' + version +'">' +
-            (
-                (classifier || extension) ?
-                    '\n  <artifact name="' + name + '"' +
-                    (extension ? ' ext="' + extension + '"' : '') +
-                    (classifier ? ' m:classifier="' + classifier + '"' : '') +
-                    ' />\n'
-                    :
-                    ''
-            ) +
-            '</dependency>'
-      }, {
-        displayName: 'Groovy Grape',
-        snippetText:
-            '@Grapes(\n' +
-            '  @Grab(group=\'' + group + '\', module=\'' + name + '\', version=\'' + version + '\'' +
-            (classifier ? ', classifier=\'' + classifier + '\'' : '' ) + ')\n' +
-            ')'
-      }, {
-        displayName: 'Leiningen',
-        snippetText:
-            '[' + group + '/' + name + ' "' + version + '"' +
-            (classifier ? ' :classifier "' + classifier + '"' : '') +
-            (extension ? ' :extension "' + extension + '"' : '') +
-            ']'
-      }, {
-        displayName: 'Apache Buildr',
-        snippetText:
-            '\'' + group + ':' + name +
-            (extension ? ':' + extension : ':jar') +
-            (classifier ? ':' + classifier : '') +
-            ':' + version + '\''
-      }, {
-        displayName: 'Maven Central Badge',
-        snippetText:
-            '[![Maven Central](https://img.shields.io/maven-central/v/' + group + '/' + name + '.svg?label=Maven%20Central)]' +
-            '(https://search.maven.org/search?q=g:%22' + group + '%22%20AND%20a:%22' + name + '%22' +
-              (classifier ? '%20AND%20l:%22' + classifier + '%22' : '') +
-            ')'
-      }, {
-        displayName: 'PURL',
-        snippetText:
-            'pkg:maven/' + group + '/' + name + '@' + version +
-            ( (classifier || extension) ?
-                '?' + (classifier ? 'classifier=' + classifier + (extension ? '&' :'') : '') +
-                (extension ? 'extension=' + extension : '')
-                :
-                ''
-            )
       }
     ];
   }
--- nexus-repository-composer/src/main/resources/static/rapture/NX/composer/util/ComposerRepositoryUrls.js
+++ nexus-repository-composer/src/main/resources/static/rapture/NX/composer/util/ComposerRepositoryUrls.js
@@ -10,18 +10,19 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-Ext.define('NX.composer.app.PluginConfig', {
-  '@aggregate_priority': 100,
+Ext.define('NX.composer.util.ComposerRepositoryUrls', {
+  '@aggregate_priority': 90,
 
+  singleton: true,
   requires: [
-    'NX.composer.app.PluginStrings',
-    'NX.composer.util.ComposerRepositoryUrls'
-  ],
-
-  controllers: [
-    {
-      id: 'NX.composer.controller.ComposerDependencySnippetController',
-      active: true
-    }
+    'NX.coreui.util.RepositoryUrls',
+    'NX.util.Url'
   ]
+}, function(self) {
+  NX.coreui.util.RepositoryUrls.addRepositoryUrlStrategy('composer', function(me, assetModel) {
+    var repositoryName = assetModel.get('repositoryName'), assetName = assetModel.get('name');
+    return NX.util.Url.asLink(
+        NX.util.Url.baseUrl + '/repository/' + encodeURIComponent(repositoryName) + '/' + encodeURI(assetName),
+        assetName);
+  });
 });
